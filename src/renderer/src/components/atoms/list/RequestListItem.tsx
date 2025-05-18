ae6ce67fd902fcfbb2d253cfa89d265dc638958a
HikaruKubota <sakaku193@gmail.com>
2025-05-18 13:33:20 +0900

    ダークモード対応

--- src/renderer/src/components/atoms/list/RequestListItem.tsx
+++ src/renderer/src/components/atoms/list/RequestListItem.tsx
@@ -21,8 +21,8 @@ export const RequestListItem: React.FC<RequestListItemProps> = ({
     className={clsx(
       'px-3 py-2 my-1 cursor-pointer border rounded flex justify-between items-center transition-colors',
       isActive
-        ? 'bg-gray-200 font-bold border-gray-400'
-        : 'bg-white font-normal border-gray-200 hover:bg-gray-100'
+        ? 'font-bold border-gray-400 dark:border-gray-600 dark:bg-gray-800 dark:text-white'
+        : 'bg-white font-normal border-gray-200 hover:bg-gray-100 dark:bg-gray-900 dark:border-gray-700 dark:hover:bg-gray-800 dark:text-gray-200'
     )}
   >
     <span>{request.name}</span>
