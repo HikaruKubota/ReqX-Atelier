11ad8f4c66d1c25ac5a604e62cad0cefbb01b30d
kubota <sakaku193@gmail.com>
2025-05-20 04:47:08 +0900

    lint警告の不要なコメントを削除

--- src/test/eslintConfig.test.ts
+++ src/test/eslintConfig.test.ts
@@ -1,5 +1,4 @@
 import { describe, it, expect } from 'vitest';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 import config from '../../eslint.config.js';
 
 describe('eslint configuration', () => {
